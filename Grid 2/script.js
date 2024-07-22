a0a572e2e916530cf415d77e1751d99d63c53635
amit1924 <amit192400@gmail.com>
2024-07-22 23:05:34 +0530

    scroll effect added on page

--- Grid 2/script.js
+++ Grid 2/script.js
@@ -4,3 +4,23 @@ const navLinks = document.querySelector(".nav-links");
 hamburger.addEventListener("click", function () {
   navLinks.classList.toggle("show");
 });
+
+const scrollDown = document.getElementById("scroll-down");
+const scrollUp = document.getElementById("scroll-up");
+const navbar = document.getElementById("navbar");
+const contact = document.getElementById("contact");
+
+scrollDown.addEventListener("click", () => {
+  contact.scrollIntoView({
+    behavior: "smooth",
+  });
+  scrollUp.style.display = "block";
+  scrollDown.style.display = "none";
+});
+scrollUp.addEventListener("click", () => {
+  navbar.scrollIntoView({
+    behavior: "smooth",
+  });
+  scrollDown.style.display = "block";
+  scrollUp.style.display = "none";
+});
